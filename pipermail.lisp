2a80719d11a748114a2e81048a178cc14cdd2ca1
Brit Butler <redline6561@gmail.com>
2013-08-20 16:05:38 -0400

    More cleanups to pipermail script.

--- pipermail.lisp
+++ pipermail.lisp
@@ -2,7 +2,7 @@
   (ql:quickload '(drakma cl-ppcre chipz)))
 
 ;; Portmanteau of Kragen and Craven...
-;; though considering the site, I don't think this scraping is contemptible behavior. :)
+;; though considering the site, I don't think scraping is contemptible behavior. :)
 (defpackage :kraven
   (:use :cl))
 
@@ -19,14 +19,14 @@
     (:thinking-out-loud . "http://lists.canonical.org/pipermail/kragen-tol/"))
   "A mapping of keywords to pipermail URLs for Kragen Sitaker's mailing lists.")
 
-(defun get-tarball-urls (string fn &optional base-url)
-  "Find all links pointing to *.gz files and call FN with each link. If BASE-URL
-is present, prepend each link with it."
-  (cl-ppcre:do-register-groups (result) ("<[A|a] href=\"(.*.gz)\">" string)
-    (funcall fn (concatenate 'string base-url result))))
+(defun do-tarball-urls (list-url fn)
+  "Find all links pointing to *.gz files and call FN with each one."
+  (cl-ppcre:do-register-groups (result)
+      ("<[A|a] href=\"(.*.gz)\">" (drakma:http-request list-url))
+    (funcall fn (concatenate 'string list-url result))))
 
 (defun download-gz-file (url directory)
-  "Download a gzipped file from URL, decompress it and store it under DIRECTORY."
+  "Download the gzipped file at URL, decompress it and store it under DIRECTORY."
   (let ((pathname (merge-pathnames (pathname-name url) directory)))
     (ensure-directories-exist pathname)
     (with-open-file (out pathname
@@ -37,16 +37,8 @@ is present, prepend each link with it."
       (chipz:decompress out 'chipz:gzip (drakma:http-request url)))
     (format t "Successfully downloaded and extracted ~a to ~a.~%~%" url pathname)))
 
-(defun download-mailing-list (list-url target-dir)
-  "Download the monthly archives at LIST-URL into TARGET-DIR and extract them."
-  (get-tarball-urls (drakma:http-request list-url)
-                    (lambda (gz) (download-gz-file gz target-dir))
-                    list-url))
-
 (defun main ()
-  "Loop over *kragen-lists*, retrieving the list of gzipped files on each list.
-Create a directory named after the list under *STORAGE-BASE*, then download and
-decompress each gzipped file into that directory and inform the user."
+  "Store the monthly archives of each kragen-mail list under *STORAGE-BASE*."
   (loop for (list-name . list-url) in *kragen-lists*
      for path = (merge-pathnames (format nil "~(~a~)/" list-name) *storage-base*)
-     do (download-mailing-list list-url path)))
+     do (do-tarball-urls list-url (lambda (gz) (download-gz-file gz path)))))
