a39bb447296d10bc2f2d1ec4aeb2d75f65fe6cef
Brit Butler <redline6561@gmail.com>
2013-08-20 11:30:40 -0400

    Cleanup kragenmail script.

--- pipermail.lisp
+++ pipermail.lisp
@@ -1,4 +1,5 @@
-(ql:quickload '(drakma cl-ppcre chipz))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload '(drakma cl-ppcre chipz)))
 
 ;; Portmanteau of Kragen and Craven...
 ;; though considering the site, I don't think this scraping is contemptible behavior. :)
@@ -8,7 +9,7 @@
 
 (in-package :kraven)
 
-(defparameter *storage-base* "/home/bsbutler/docs/books/people/kragen/"
+(defparameter *storage-base* "/home/redline/Downloads/kragen/"
   "The directory to store the mailing list transcripts in.")
 
 (defparameter *kragen-lists*
@@ -17,36 +18,33 @@
     (:hacks . "http://lists.canonical.org/pipermail/kragen-hacks/")
     (:journal . "http://lists.canonical.org/pipermail/kragen-journal/")
     (:thinking-out-loud . "http://lists.canonical.org/pipermail/kragen-tol/"))
-  "A mapping of keywords to pipermail URLs for Kragen Sitaker's mailing lists,
-serving as his blog and thinking grounds.")
+  "A mapping of keywords to pipermail URLs for Kragen Sitaker's mailing lists.")
 
-(defun get-tarball-urls (string)
-  "Find all links pointing to *.gz files."
-  (let ((results nil))
-    (cl-ppcre:do-register-groups (result)
-        ("<[A|a] href=\"(.*.gz)\">" string)
-      (push result results))
-    results))
+(defun get-tarball-urls (string fn &optional base-url)
+  "Find all links pointing to *.gz files and call FN with each link. If BASE-URL
+is present, prepend each link with it."
+  (cl-ppcre:do-register-groups (result) ("<[A|a] href=\"(.*.gz)\">" string)
+    (funcall fn (concatenate 'string base-url result))))
 
-;; TODO
-;; Read LOOP for Black Belts and apply.
-;; Seems I don't understand loop :with scoping.
+(defun download-gz-file (url directory)
+  "Download a gzipped file from URL, decompress it and store it under DIRECTORY."
+  (let ((pathname (merge-pathnames (pathname-name url) directory)))
+    (with-open-file (out pathname
+                         :direction :output
+                         :element-type '(unsigned-byte 8)
+                         :if-exists :supersede
+                         :if-does-not-exist :create)
+      (chipz:decompress out 'chipz:gzip (http-request url)))
+    (format t "Successfully downloaded and extracted ~a to ~a.~%" url pathname)))
+
 (defun main ()
   "Loop over *kragen-lists*, retrieving the list of gzipped files on each list.
 Create a directory named after the list under *STORAGE-BASE*, then download and
 decompress each gzipped file into that directory and inform the user."
   (loop for (name . list-url) in *kragen-lists*
      with directory = (string-downcase (string name))
-     with gz-files = (get-tarball-urls (http-request list-url))
-     with path = (merge-pathnames (concatenate 'string directory "/") *storage-base*)
+     with path = (merge-pathnames (format nil "~a/" directory) *storage-base*)
      do (ensure-directories-exist path)
-        (loop for gz-file in gz-files
-           do (let ((out-file (merge-pathnames (pathname-name gz-file) path)))
-                (unless (probe-file out-file)
-                (with-open-file (out out-file :direction :output
-                                     :element-type '(unsigned-byte 8)
-                                     :if-exists :supersede)
-                  (chipz:decompress out 'chipz:gzip
-                                    (http-request (concatenate 'string list-url gz-file))))
-                (format t "Successfully downloaded and extracted ~
-~a to ~a.~%" gz-file out-file))))))
+        (get-tarball-urls (http-request list-url)
+                          (lambda (gz) (download-gz-file gz path))
+                          list-url)))
